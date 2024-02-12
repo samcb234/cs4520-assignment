333e72217530fab5abacfb47f375fc3e7151e799
samblock <block.sa@northeastern.edu>
2024-02-12 15:33:56 -0500

    added toast and cleaned up login page

--- app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -19,11 +20,17 @@ class LoginFragment: Fragment(R.layout.login_layout) {
 
         view.findViewById<Button>(R.id.loginButton).setOnClickListener(View.OnClickListener {
 
-            val username = view.findViewById<EditText>(R.id.usernameText).text.toString()
-            val password = view.findViewById<EditText>(R.id.passwordText).text.toString()
-            if(username.equals("admin") && password.equals("admin")){
+            val username = view.findViewById<EditText>(R.id.usernameText)
+            val password = view.findViewById<EditText>(R.id.passwordText)
+            if(username.text.toString() == ("admin") && password.text.toString() == ("admin")){
                 findNavController().navigate(R.id.action_loginFragment_to_productListFragment)
+            } else {
+                val toast = Toast.makeText(context, "Please enter correct username and password", Toast.LENGTH_SHORT)
+                toast.show()
             }
+
+            username.setText("")
+            password.setText("")
         })
         return view
     }
